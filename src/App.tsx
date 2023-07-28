2d6ebc5352e1fd52e3cd7ea48fc70c3a73814f1b
Quang Nguyen <quangnguyen.4298@gmail.com>
2023-07-28 22:39:52 +0700

    Counter with flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState<number>(0);
+  const [isStart, setIsStart] = useState<boolean>(false);
+  const [oddNumberList, setOddNumberList] = useState<Array<number>>([]);
+  const [evenNumberList, setEvenNumberList] = useState<Array<number>>([]);
+  // const counter = useRef<any>(null);
+
+  const isEven = (number: number) => number % 2 === 0;
+
+  const onStart = () => {
+    setIsStart(true);
+  };
+
+  const onGetNumber = () => {
+    setIsStart(false);
+    if (isEven(number)) {
+      setEvenNumberList([...evenNumberList, number]);
+    } else {
+      setOddNumberList([...oddNumberList, number]);
+    }
+  };
+
+  useEffect(() => {
+    // if (isStart) {
+    //   counter.current = setInterval(() => {
+    //     setNumber((prevNumber) => prevNumber + 1);
+    //   }, 1000);
+    // } else {
+    //   clearInterval(counter.current);
+    // }
+    let timer: any;
+    if (isStart) {
+      timer = setInterval(() => {
+        setNumber((prevNumber) => prevNumber + 1);
+      }, 1000);
+    }
+    // else {
+    //   clearInterval(timer);
+    // }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isStart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <div className="flex-container">
+        <div className="flex-item"></div>
+        <div className="flex-item">
+          <p>
+            {!isStart ? (
+              <button onClick={onStart}> start</button>
+            ) : (
+              <button onClick={onGetNumber}>get number</button>
+            )}
+          </p>
+          <p>{number}</p>
+        </div>
+        <div className="flex-item"></div>
+      </div>
+      <div className="flex-container">
+        <div className="flex-item">
+          <div className="sub-box">
+            {oddNumberList.map((item) => (
+              <p>{item}</p>
+            ))}
+          </div>
+        </div>
+        <div className="flex-item"></div>
+        <div className="flex-item">
+          <div className="sub-box">
+            {evenNumberList.map((item) => (
+              <p>{item}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +92,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
